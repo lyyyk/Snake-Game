030b8b9010a4d8b68d400100fe241d3f6607461b
lyyyk <107653002+lyyyk@users.noreply.github.com>
2022-06-17 22:42:03 -0700

    Update FoodManager.cpp

--- FoodManager.cpp
+++ FoodManager.cpp
@@ -1,12 +1,14 @@
 #include "FoodManager.h"
 #include <cstdlib>
 
+//determines the position of the food
 void FoodManager::UpdatePos() {
 	int x = leftTop.x + (rand() % (rightBottom.x - leftTop.x));
 	int y = leftTop.y + (rand() % (rightBottom.y - leftTop.y));
 	foodPosition.Set(x, y);
 }
 
+//member function that sets and updates the position of the food
 FoodManager::FoodManager() {
 	leftTop = Point (1, 1);
 	rightBottom = Point(99, 24);
